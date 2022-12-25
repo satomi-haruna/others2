04ad3d2e4aee2d9ccd267fcfc5e44b009ecef3ab
satomi-haruna <satomi.haruna1124@gmail.com>
2022-12-25 04:02:14 +0000

    lesson6-2

--- ruby/lesson6-1.rb
+++ ruby/lesson6-1.rb
@@ -2,10 +2,11 @@
 puts "計算をはじめます"
 puts "2つの値を入力してください"
 
-number1 = gets
-number2 = gets
+number1 = gets #解答 number1 = gets.to_i
+number2 = gets #解答 number2 = gets.to_i
 
 puts "計算結果を出力します"
 
 puts "#{number1.to_i}*#{number2.to_i}=#{number1.to_i * number2.to_i}"
+#解答 puts "#{number1}*#{number2}=#{number1*number2}"
 puts "計算を終了します"
---
+++ ruby/lesson6-2.rb
@@ -0,0 +1,29 @@
+# 整数値の入力を受け、それらを四則演算の結果を出力する
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+
+times = gets.to_i
+
+i = 1
+while i <= times+1 do
+  if i == times+1 #解答 不要
+    puts "計算を終了します" #解答 不要
+    break #解答 不要
+  end #解答 不要
+
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  number3 = gets.to_i
+  number4 = gets.to_i
+  puts "a=#{number3}"
+  puts "b=#{number4}"
+  puts "計算結果を出力します"
+  puts "#{number3}+#{number4}=#{number3+number4}"
+  puts "#{number3}-#{number4}=#{number3-number4}"
+  puts "#{number3}*#{number4}=#{number3*number4}"
+  puts "#{number3}/#{number4}=#{number3/number4}"
+
+  i += 1
+end
+
+# 解答 puts "計算を終了します"
