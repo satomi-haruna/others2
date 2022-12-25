41f506f063232188b1670f8ad83b510ff4323fbe
satomi-haruna <satomi.haruna1124@gmail.com>
2022-12-25 03:15:52 +0000

    lesson6-1

--- ruby/lesson6-1.rb
+++ ruby/lesson6-1.rb
@@ -1,1 +1,11 @@
 # 2つの整数値の入力を受け付け、それらの掛け算の結果を出力する
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+
+number1 = gets
+number2 = gets
+
+puts "計算結果を出力します"
+
+puts "#{number1.to_i}*#{number2.to_i}=#{number1.to_i * number2.to_i}"
+puts "計算を終了します"
